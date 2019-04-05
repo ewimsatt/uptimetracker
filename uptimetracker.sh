7ce1e751ec844112c218661e4abf77091922f315
ewimsatt <ewimsatt@gmail.com>
2019-04-05 17:40:50 +0000

    split config out into it's own conf file and added status code checking

--- uptimetracker.sh
+++ uptimetracker.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+cd $(dirname $0)
+source config/uptimetracker.conf
 cat config/sitelist.txt | sort -u | while read site
 do 
   format="%{time_total}"
+  sitemap="/sitemap.xml"
   time="$(curl -w "$format" -o /dev/null -s "$site")"
   short="${time:0:1}"
-  if [ "$short" -gt 4 ]; then
-  echo "Placeholder for code that will send an email for a downed site."; 
+  status="$(curl -I "$site$sitemap" | grep HTTP | awk '{print $2}')"
+ if [ "$short" -gt 4 ]; then
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"'$site' is taking more than 4 seconds to load."}' $slack && echo "$site is slow. Slack notified on $(date)" >> $log
   elif [ "$short" -le 4 ]; then
-  echo "$site is ok. Date: $(date). Page Load Time: $time seconds." >> logs/uptime.log;
-  fi
+  echo "$site is ok. Date: $(date). Page Load Time: $time seconds." >> $logs;
+ fi
+ if [[ "$status" == "40"* ]]; then
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"'$site' is reporting 404."}' $slack && echo "$site is down. Slack notified on $(date)" >> $logs;
+  elif [[ "$status" != "40"* ]]; then
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"'$site' is reporting ok (testing script)."}' $slack;
+ fi
 done
